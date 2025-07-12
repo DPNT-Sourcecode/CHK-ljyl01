9e877fdfa9531747db58eff1982491551c3378f2
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-07-12 19:03:14 +0000

    Sat Jul 12 19:03:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,22 @@ class CheckoutSolution:
     def checkout(self, skus):
         prices = {
             "A": 50,
-            
+            "B": 30,
+            "C": 20,
+            "D": 15,
+        }
+        item_counts = {
+            "A": 0,
+            "B": 0,
+            "C": 0,
+            "D": 0,
         }
         for sku in skus:
+            if sku not in prices:
+                return -1
+            item_counts[sku] += 1
 
-        return -1
+
 
 
 
