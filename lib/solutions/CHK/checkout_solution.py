aa5962894b4609136e65342a73fe67896e546f60
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-07-12 18:51:14 +0000

    Sat Jul 12 18:51:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,7 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         # don't know the exact format of the skus string yet
-        return total
+        # need to run solution to see test failures to understand format
+        return -1
+
 
