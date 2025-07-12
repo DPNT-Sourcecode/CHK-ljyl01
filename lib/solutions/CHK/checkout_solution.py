9bf13a68c9a62755aa4019d39ae75bf3789787ad
sbx_user1051 <sbx_user1051@169.254.52.125>
2025-07-12 21:08:45 +0000

    Sat Jul 12 21:08:45 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -77,7 +77,7 @@ class CheckoutSolution:
             order_from_most_expensive = sorted(sku_group, key=lambda x: self.PRICES[x], reverse=True)
             for _ in range(num_times_to_apply_discount * offer[0]):
                 for free_sku in order_from_most_expensive:
-                    while item_counts[free_sku] > 0:
+                    if item_counts[free_sku] > 0:
                         item_counts[free_sku] -= 1
                     # else:
                     #     order_from_most_expensive.remove(free_sku)
@@ -96,6 +96,7 @@ class CheckoutSolution:
             item_prices[sku] += item_counts[sku] * self.PRICES[sku]
 
         return total_price + sum(item_prices.values())
+
 
 
 
